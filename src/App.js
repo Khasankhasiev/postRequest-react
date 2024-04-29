9d772a7f6c21fd22f6b3ed7c1a3d5328cff4c22e
Khasan <mr.khasiev@list.ru>
2024-04-29 20:43:50 +0300

    prepare

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { useState } from "react";
 
 function App() {
-  const state = useState(0);
 
-  let likes = 0;
 
   return (
-    <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={() => (likes += 1)}>Increment</button>
-      <button onClick={() => (likes -= 1)}>Decrement</button>
-    </div>
+
   );
 }
 
